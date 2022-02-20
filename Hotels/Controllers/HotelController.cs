341ca6d18041011b0bb8e63be795e2816a69705d
Guilherme Pinheiro <guilhermemarkevis@hotmail.com>
2022-02-20 02:46:25 -0300

    Criando CRUD para hotéis

---
+++ Hotels/Controllers/HotelController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Hotels.Models;
+using Hotels.Models.ViewModels;
+using Microsoft.AspNetCore.Cors;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace Hotels.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly HotelsContext _context;
+
+        public HotelController(HotelsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Hotel.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(HotelVM hotelVM)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Hotel hotel = new Hotel();
+                hotel.Nome = hotelVM.Nome;
+                hotel.CNPJ = hotelVM.CNPJ;
+                hotel.Endereco = hotelVM.Endereco;
+                hotel.Descricao = hotelVM.Descricao;
+
+                if (hotelVM.Foto != null)
+                    hotel.Foto = ConverterFotoParaBytes(hotelVM.Foto);
+
+                _context.Add(hotel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hotelVM);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotel.FindAsync(id);
+
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            HotelVM hotelVM = new HotelVM()
+            {
+                Id = hotel.Id,
+                Nome = hotel.Nome,
+                CNPJ = hotel.CNPJ,
+                Endereco = hotel.Endereco,
+                Descricao = hotel.Descricao
+            };
+
+            ViewBag.FotoAtual = hotel.Foto;
+
+            return View(hotelVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, HotelVM hotelVM)
+        {
+            if (id != hotelVM.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Hotel hotel = new Hotel()
+                    {
+                        Id = hotelVM.Id,
+                        Nome = hotelVM.Nome,
+                        CNPJ = hotelVM.CNPJ,
+                        Endereco = hotelVM.Endereco,
+                        Descricao = hotelVM.Descricao
+                    };
+                    if (hotelVM.Foto == null)
+                        hotel.Foto = _context.Hotel.Where(h => h.Id == id).Select(h => h.Foto).FirstOrDefault();
+                    else
+                        hotel.Foto = ConverterFotoParaBytes(hotelVM.Foto);
+                    
+
+                    _context.Update(hotel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HotelExists(hotelVM.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hotelVM);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hotel = await _context.Hotel.FindAsync(id);
+            _context.Hotel.Remove(hotel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HotelExists(int id)
+        {
+            return _context.Hotel.Any(e => e.Id == id);
+        }
+
+        public byte[] ConverterFotoParaBytes(IFormFile foto)
+        {
+            using (var ms = new MemoryStream())
+            {
+                foto.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ Hotels/Controllers/QuartoController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Hotels.Models;
+
+namespace Hotels.Controllers
+{
+    public class QuartoController : Controller
+    {
+        private readonly HotelsContext _context;
+
+        public QuartoController(HotelsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Quarto
+        public async Task<IActionResult> Index()
+        {
+            var hotelsContext = _context.Quarto.Include(q => q.Hotel);
+            return View(await hotelsContext.ToListAsync());
+        }
+
+        // GET: Quarto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quarto = await _context.Quarto
+                .Include(q => q.Hotel)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (quarto == null)
+            {
+                return NotFound();
+            }
+
+            return View(quarto);
+        }
+
+        // GET: Quarto/Create
+        public IActionResult Create()
+        {
+            ViewData["HotelId"] = new SelectList(_context.Hotel, "Id", "Id");
+            return View();
+        }
+
+        // POST: Quarto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,HotelId,Nome,NumeroOcupantes,NumeroAdultos,NumeroCriancas,Preco,Foto")] Quarto quarto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(quarto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HotelId"] = new SelectList(_context.Hotel, "Id", "Id", quarto.HotelId);
+            return View(quarto);
+        }
+
+        // GET: Quarto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quarto = await _context.Quarto.FindAsync(id);
+            if (quarto == null)
+            {
+                return NotFound();
+            }
+            ViewData["HotelId"] = new SelectList(_context.Hotel, "Id", "Id", quarto.HotelId);
+            return View(quarto);
+        }
+
+        // POST: Quarto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,HotelId,Nome,NumeroOcupantes,NumeroAdultos,NumeroCriancas,Preco,Foto")] Quarto quarto)
+        {
+            if (id != quarto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(quarto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuartoExists(quarto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HotelId"] = new SelectList(_context.Hotel, "Id", "Id", quarto.HotelId);
+            return View(quarto);
+        }
+
+        // GET: Quarto/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quarto = await _context.Quarto
+                .Include(q => q.Hotel)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (quarto == null)
+            {
+                return NotFound();
+            }
+
+            return View(quarto);
+        }
+
+        // POST: Quarto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var quarto = await _context.Quarto.FindAsync(id);
+            _context.Quarto.Remove(quarto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuartoExists(int id)
+        {
+            return _context.Quarto.Any(e => e.Id == id);
+        }
+    }
+}
